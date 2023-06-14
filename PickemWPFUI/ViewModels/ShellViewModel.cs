3c14bcdc8a04c6eb7d4b0f8415c7aa2b6a681fd9
Jerry Jacobson <joeyj96@live.com>
2023-06-13 19:12:35 -0500

    Initializes WPF Project with Bootstrapper
    Beginning of front-end WPF project using the MVVM
    project format

---
+++ PickemWPFUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickemWPFUI.ViewModels
+{
+    public class ShellViewModel
+    {
+    }
+}
