ac1470a2335515547b31783f73a841d3934b4d20
Jerry Jacobson <joeyj96@live.com>
2023-06-24 19:36:27 -0500

    Adds functionality for disabling the LogIn button via CanLogIn property
    CanLogIn is wired up through Caliburn.Micro, and is called when there is a property change (Username/Password)

--- PickemWPFUI/ViewModels/LoginViewModel.cs
+++ PickemWPFUI/ViewModels/LoginViewModel.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
 
 namespace PickemWPFUI.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : Screen
     {
-    }
+		private string _userName;
+        private string _password;	
+
+        public string Username
+		{
+			get { return _userName; }
+			set {
+				_userName = value;
+				NotifyOfPropertyChange(() => Username);
+				NotifyOfPropertyChange(() => CanLogIn);
+			}
+		}
+
+		public string Password
+		{
+			get { return _password; }
+			set
+			{
+				_password = value;
+				NotifyOfPropertyChange(() => Password);
+				NotifyOfPropertyChange(() => CanLogIn);
+			}
+		}
+
+		public bool CanLogIn
+		{
+			get
+			{
+                bool output = false;
+
+                if (!String.IsNullOrEmpty(Username) && !String.IsNullOrEmpty(Password))
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+		}
+
+		public void LogIn(string userName, string password)
+		{
+			Console.WriteLine("You logged in!");
+		}
+	}
 }
