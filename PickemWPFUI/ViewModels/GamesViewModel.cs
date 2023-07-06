e20d97a6f4fad71a4a1c1750fd39fbffff39233b
Jerry Jacobson <joeyj96@live.com>
2023-07-05 21:17:36 -0500

    Successfull login redirects to games list
    WIP games list autopopulates from DB via API call

--- PickemWPFUI/ViewModels/GamesViewModel.cs
+++ PickemWPFUI/ViewModels/GamesViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Caliburn.Micro;
+using PickemWPFUI.Helpers;
+using PickemWPFUI.Models;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -14,6 +16,15 @@ namespace PickemWPFUI.ViewModels
     {
         private StackPanel _gamesStackPanel;
 
+        private IAPIHelper _apiClient;
+        private IEventAggregator _events;
+
+        public GamesViewModel(IEventAggregator events, IAPIHelper apiClient)
+        {
+            _apiClient = apiClient;
+            _events = events;
+        }
+
         public StackPanel GamesStackPanel
         {
             get { return _gamesStackPanel; }
@@ -22,38 +33,37 @@ namespace PickemWPFUI.ViewModels
         // Automatically populate list of games using the games table
         public void InitializeList()
         {
-            // string is just a placeholder here -- will be a 'Game' object. This comes from the API that the admin populates manually
-            List<string> gamesToPopulate = new List<string>();
+            // This list comes from a table that the admin populates manually
+            List<Game> gamesToPopulate = new List<Game>();
 
-            // For each 'game' in gamesToPopulate, get the gameId and pass it to _gamesStackPanel.Find()
-            //foreach (var gameToPopulate in gamesToPopulate)
-            //{
-            //    string gameId = gameToPopulate.gameId;
+            foreach (var gameToPopulate in gamesToPopulate)
+            {
+                string gameId = gameToPopulate.gameId;
 
-            //    DockPanel game = (DockPanel)_gamesStackPanel.FindName(gameId);
+                DockPanel game = (DockPanel)_gamesStackPanel.FindName(gameId);
 
-            //    List<Button> buttons = game.Children.OfType<Button>().ToList();
+                List<Button> buttons = game.Children.OfType<Button>().ToList();
 
-            //    buttons[0].Content = $"{gameToPopulate.Home} {gameToPopulate.HomeSpread}";
-            //    buttons[1].Content = $"{gameToPopulate.Away} {gameToPopulate.AwaySpread}";
-            //}
+                buttons[0].Content = $"{gameToPopulate.Home} {gameToPopulate.HomeSpread}";
+                buttons[1].Content = $"{gameToPopulate.Away} {gameToPopulate.AwaySpread}";
+            }
         }
 
-        //public void Clicked(object sender, EventArgs e)
-        //{
-        //    Button btn = (Button)sender;
-        //    var btnContent = (System.Xml.XmlElement)btn.Content;
+        public void Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            var btnContent = (System.Xml.XmlElement)btn.Content;
 
-        //    string[] teamInfo = btnContent.InnerText.Split(' ');
-            
+            string[] teamInfo = btnContent.InnerText.Split(' ');
 
-        //    // TODO: Process selection
-        //    // Store in a binding list?
-        //}
 
-        //public void SubmitPicks(object sender, EventArgs e)
-        //{
-        //    // However we stored the pick set, fire that off to the API
-        //}
+            // TODO: Process selection
+            // Store in a binding list?
+        }
+
+        public void SubmitPicks(object sender, EventArgs e)
+        {
+            // However we stored the pick set, fire that off to the API
+        }
     }
 }
